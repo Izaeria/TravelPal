10c3ba62f15487e5a487ec2a4f814fe972746051
Izabell <izabell.hallgren@gmail.com>
2024-02-16 21:24:38 +0100

    Created Classes and Managers

---
+++ TravelPal/Classes/IUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TravelPal.Classes
+{
+    public class IUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Location(country)
+
+
+    }
+
+    public class User : IUser
+    {
+
+        public List<Travel> travels = new();
+
+    }
+
+    public class Admin : IUser
+    {
+
+    }
+
+    public MyClass()
+    { 
+    }
+
+
+}
